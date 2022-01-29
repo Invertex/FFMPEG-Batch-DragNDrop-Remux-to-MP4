bc4e321d8f88a19143f85e736c81511ab1c986db
Dabbles in too many things <mitch.zais@gmail.com>
2022-01-29 06:22:10 -0800

    Fix batch folder processing not replacing file extension
    Fixed folder processing so that it isn't retaining the old file extension.
    Also added a feature to copy all the files that couldn't be remuxed to a separate folder structure so you can manually convert those and then copy the folder structure over afterwards.

--- Drag_Files_Here_to_Remux_to_MP4.bat
+++ Drag_Files_Here_to_Remux_to_MP4.bat
@@ -10,7 +10,7 @@ IF (%1) == () GOTO NOFILES
 
 SET BPATH=%~dp0
 SET /a processed=0
-
+SET /a copied=0
 :PROCESSINPUT
 IF (%1) == () GOTO END
 
@@ -29,23 +29,33 @@ goto PROCESSINPUT
 
 :PROCESSFOLDER <folderPath>
 ECHO PROC FOLDER
-setlocal enabledelayedexpansion
 set convPath=%~1_Converted
 set origPath=%~1
 
 if not exist "%convPath%" MD "%convPath%"
 ECHO %convPath%
 for /f "delims=" %%a in ('dir "%~1" /b /s /A:-D') do (
-	set fpath=%%a
+	set fpath=%%~dpna
 	call set fpath=%%fpath:!origPath!=!convPath!%%
-	call :PROCESSFILE "%%a" "!fpath!"
+	call :PROCESSFILE "%%a" "!fpath!.mp4"
 )
 exit /b 0
 
-:PROCESSFILE <filePath> <newPath>
-SET /a processed+=1
+:PROCESSFILE <filePath> <newPath> 
 if not exist "%~dp2" MD "%~dp2"
 "%BPATH%ffmpeg.exe" -err_detect ignore_err -i "%~1" -c copy "%~2"
+
+if "%~z2"=="0" (
+    del /f "%~2"
+    set word=NonRemuxable\
+    set str=%~dp2
+    call set str=%%str:Converted\=!word!%%
+    if not exist "!str!" MD "!str!"
+    xcopy "%~1" "!str!"
+    SET /a copied+=1
+    call :IsFolderEmpty "%~dp2"
+    if ERRORLEVEL 1 ( rmdir "%~dp2" )
+) else ( SET /a processed+=1 )
 exit /b 0
 
 :NOFILES
@@ -57,6 +67,7 @@ exit
 ECHO.
 ECHO.
 ECHO !processed! Files converted!
+if !copied! NEQ 0 ( ECHO !copied! Files that can't be remuxed were copied to "NonRemuxable" folder. You'll want to actually re-encode these video files.)
 ECHO.
 pause
 exit
@@ -72,4 +83,10 @@ exit
 		set isFolder=1
 	)
 	exit /b !isFolder!
+)
+
+:IsFolderEmpty <pathVar>
+(	
+	for %%I in ("%~f1\*.*") do exit /b 0 
+	exit /b 1
 )
